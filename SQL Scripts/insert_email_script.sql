741ee6e9219f6f3547e6a4a5dfd21c2be8ba1666
Rashmi Desai <rash.desai09@gmail.com>
2023-01-21 11:56:10 +0530

    Add files via upload

---
+++ SQL Scripts/insert_email_script.sql
@@ -0,0 +1,68 @@
+
+INSERT INTO [dbo].[EmailRecipients](
+    SystemCode,
+    DatasetName,
+    SystemInfoID,
+    FirstName,
+    LastName,
+    EmailAddress
+)VALUES(
+    'OWS',
+    'Sales',
+    1,
+    'Rashmi',
+    'D',
+    'rash.desai09@gmail.com'
+)
+GO
+
+INSERT INTO [dbo].[EmailRecipients](
+    SystemCode,
+    DatasetName, 
+    SystemInfoID,
+    FirstName,
+    LastName,
+    EmailAddress
+)VALUES(
+    'OWS',
+    'Sales',
+     1,
+    'Aayu',
+    'B',
+    'rashbeats@gmail.com'
+)
+GO
+
+INSERT INTO [dbo].[EmailRecipients](
+    SystemCode,
+    DatasetName,
+    SystemInfoID,
+    FirstName,
+    LastName,
+    EmailAddress
+)VALUES(
+    'OWS',
+    'SalesRef',
+    2,
+    'Rashmi',
+    'D',
+    'rash.desai09@gmail.com'
+)
+GO
+
+INSERT INTO [dbo].[EmailRecipients](
+    SystemCode,
+    DatasetName,
+    SystemInfoID, 
+    FirstName,
+    LastName,
+    EmailAddress
+)VALUES(
+    'OWS',
+    'SalesRef',
+    2,
+    'Aayu',
+    'B',
+    'rashbeats@gmail.com'
+)
+GO
